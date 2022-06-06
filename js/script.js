eddc4bd0f62437cd2f508442024d3413a2be610a
Electr0Nick <kozhakin.maks@gmail.com>
2022-06-06 13:41:40 +0300

    add header & footer navigation. add animated logo

--- js/script.js
+++ js/script.js
@@ -25,53 +25,86 @@ let pageSlider = new Swiper(`.swiper`, {
     init: false,
     on: {
         init: function () {
-            menuSlider();
+            menuNav(barNavList);
+            menuNav(headerNavList);
+            menuNav(footerNavList);
         },
         slideChange: function () {
-            setTimeout(hideNav, 1100)
-            menuSliderRemove();
-            navList[pageSlider.realIndex].classList.add(`active`);
+            setTimeout(hideNav, 1100);
+            menuNavRemove();
+            removeBarArrows()
+            barNavList[pageSlider.realIndex].classList.add(`active-nav`);
+            headerNavList[pageSlider.realIndex].classList.add(`active-nav`);
+            footerNavList[pageSlider.realIndex].classList.add(`active-nav`);
         },
     },
 });
 
-// ----------------------------------------------------------------hide navigation----------------------------------------------------------------
+// ----------------------------------------------------------------hide navigation bar----------------------------------------------------------------
 let navBlock = document.querySelector(`.navigation`);
 function hideNav() {
     if (navBlock) {
         if (pageSlider.realIndex === 0 || pageSlider.realIndex === (pageSlider.slides.length - 1)) {
-            navBlock.classList.remove(`active`);
+            navBlock.classList.remove(`active-bar`);
         } else {
-            navBlock.classList.add(`active`);
+            navBlock.classList.add(`active-bar`);
         }
     }
 }
 
-// ----------------------------------------------------------------main navigation----------------------------------------------------------------
-let navList = document.querySelectorAll(`.navigation__link`); // находим все навигационные ссылки
-function menuSlider() {
-    if (navList.length > 0) { // проверка
-        navList[pageSlider.realIndex].classList.add(`active`); // добавляем стили для ссылки актуального слайда
-        for(let i = 0; i < navList.length; i++) { // перебор массива
-            const navLink = navList[i];
-            navLink.addEventListener(`click`, function(e) { // вешаем обработчик на каждую ссылку
-                menuSliderRemove(); // убираем стили с предыдущей ссылки
+// ----------------------------------------------------------------navigation----------------------------------------------------------------
+let barNavList = document.querySelectorAll(`.navigation__link`);
+let headerNavList = document.querySelectorAll(`.header-nav__link`);
+let footerNavList = document.querySelectorAll(`.footer-nav__link`);
+
+function menuNav(linkArray) {
+    if (linkArray.length > 0) { // проверка
+        linkArray[pageSlider.realIndex].classList.add(`active-nav`); // добавляем стили для ссылки актуального слайда
+        for(let i = 0; i < linkArray.length; i++) { // перебор массива
+            const link = linkArray[i];
+            link.addEventListener(`click`, function(e) { // вешаем обработчик на каждую ссылку
+                menuNavRemove(); // убираем стили с предыдущей ссылки
                 pageSlider.slideTo(i, 1000);  // переходим на новый слайд
-                navLink.classList.add(`active`); // добавляем стили для ссылки нового слайда
+                link.classList.add(`active-nav`); // добавляем стили для ссылки нового слайда
                 e.preventDefault(); // убираем действие ссылки по умолчанию
             });
         }
     }
 }
-function menuSliderRemove() { // функция для очистки стиля ссылки
-    let navLinkActive = document.querySelector(`.navigation__link.active`); // находим ссылку со ститями
-    if (navLinkActive) {
-        navLinkActive.classList.remove(`active`); // убираем стили со ссылки
+function menuNavRemove() { // функция для очистки стиля ссылки
+    let linkArrayActive = document.querySelectorAll(`.active-nav`);// находим ссылку со ститями
+    if (linkArrayActive.length > 0) {
+        for(let i = 0; i < linkArrayActive.length; i++) {
+            const activelink = linkArrayActive[i];
+            activelink.classList.remove(`active-nav`); // убираем стили со ссылки
+        }
     }
 }
 pageSlider.init();
 
-// ----------------------------------------------------------------nav arrows----------------------------------------------------------------
+// ----------------------------------------------------------------navigation bar arrows----------------------------------------------------------------
+let upBarArrow = document.querySelector(`.navigation__arrow_up`);
+let downBarArrow = document.querySelector(`.navigation__arrow_down`);
+
+function removeBarArrows() {
+    if (upBarArrow) {
+        if (pageSlider.realIndex === 0) {
+            upBarArrow.classList.add(`hide-arrow`);
+        } else {
+            upBarArrow.classList.remove(`hide-arrow`);
+        }
+    }
+    if (downBarArrow) {
+        if (pageSlider.realIndex === pageSlider.slides.length - 1) {
+            downBarArrow.classList.add(`hide-arrow`);
+        } else {
+            downBarArrow.classList.remove(`hide-arrow`);
+        }
+    }
+}
+
+
+// ----------------------------------------------------------------firstscreen & lastscreen arrows----------------------------------------------------------------
 let downArrow = document.querySelector(`.navbutton_down`);
 let upArrow = document.querySelector(`.navbutton_up`);
 if (downArrow) {
